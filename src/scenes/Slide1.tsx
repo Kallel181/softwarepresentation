8bb787f87185f9ed1e3dd27606cb1ecf17aba327
Kallel181 <86331656+Kallel181@users.noreply.github.com>
2025-05-28 20:07:58 -0300

    reworked File Structure

--- src/scenes/Slide1.tsx
+++ src/scenes/Slide1.tsx
@@ -8,73 +8,33 @@ export default makeScene2D(function* (view) {
   view.fill('#101214');
 
   // Refs
-  const title = createRef<Txt>();
-  const ofuscTitle = createRef<Txt>();
-  const ofuscText = createRef<Txt>();
-  const diversTitle = createRef<Txt>();
-  const diversText = createRef<Txt>();
-  const pageNumber = createRef<Txt>();
+  const paper = createRef<Txt>();
 
 
-  // Textos
-  const title_final_text = "O que é Ofuscação e Diversificação?";
-  const ofusc_final_text = "Ofuscação é o processo de embaralhar o código e torná-lo ininteligível (mas ainda funcional), a fim de dificultar a engenharia reversa. (Hosseinzadeh et al., 2018, p.72)";
-  const divers_final_text = "Diversificação é a mudança das interfaces internas e da estrutura do software para gerar versões únicas e diversificadas dele. Os usuários recebem instâncias únicas que funcionam da mesma forma. (Hosseinzadeh et al., 2018, p.7)";
+  //Texts
+  const paper_name = "S. Hosseinzadeh, S. Rauti, S. Laurén, J.-M. Mäkelä, J. Holvitie, S. Hyrynsalmi, and V. Leppänen,“Diversification and obfuscation techniques for software security: A systematic literature review,” Elsevier, 2018"
 
 
   yield* slideTransition(Direction.Right,1);
 
+
   // Layout principal
   view.add(
-    <Layout layout direction="column" alignItems="start" gap={60} padding={60} width={1920} height={1080} wrap={'wrap'}>
-      <Txt
+    <Layout layout direction="column" alignItems="center" justifyContent={"center"} gap={60} padding={60} width={1920} height={1080} wrap={'wrap'}>
+      <Txt 
         {...styles.DefaultTitleStyle}
-        ref={title}
-        text={title_final_text}
+        ref={paper}
+        text={paper_name}
+        fontSize={60}
+        textAlign={'center'}
       />
-      <Layout layout direction="column" gap={100}>
-        <Layout layout direction="column" gap={20}>
-          <Txt
-            {...styles.DefaultSubTitleStyle}
-            ref={ofuscTitle}
-            text="Ofuscação"
-          />
-          <Txt
-            {...styles.DefaultNormalText}
-            ref={ofuscText}
-            text={ofusc_final_text}
-          />
-        </Layout>
-        <Layout layout direction="column" gap={20} alignItems={'start'}> 
-          <Txt
-            textWrap={true}
-            ref={diversTitle}
-            text="Diversificação"
-            fontFamily="Cal Sans"
-            fontWeight={700}
-            fill="#ffffff"
-            fontSize={64}
-            opacity={0}
-          />
-          <Txt
-            textWrap={true}
-            ref={diversText}
-            text={divers_final_text}
-            fontFamily="Cal Sans"
-            fontWeight={400}
-            fill="#ededed"
-            fontSize={42}
-            opacity={0}
-          />
-        </Layout>
-      </Layout>
     </Layout>
   );
+
 
   // Número da página no canto inferior direito
   view.add(
     <Txt
-      ref={pageNumber}
       text="1"
       fontFamily="Cal Sans"
       fontWeight={300}
@@ -87,19 +47,7 @@ export default makeScene2D(function* (view) {
 
   // Animações
   yield* beginSlide('Animation 1');
-  yield* revealTextScramble(title(), title_final_text, 0.03);
-
-  yield* beginSlide('Animation 2');
-  yield* revealTextScramble(ofuscTitle(), "Ofuscação", 0.03);
-
-  yield* beginSlide('Animation 3');
-  yield* ofuscText().opacity(1, 0.5);
-
-  yield* beginSlide('Animation 4');
-  yield* revealTextScramble(diversTitle(), "Diversificação", 0.03);
-
-  yield* beginSlide('Animation 5');
-  yield* diversText().opacity(1, 0.5);
+  yield* revealTextScramble(paper(), paper_name, 0.01);
 
 
 
