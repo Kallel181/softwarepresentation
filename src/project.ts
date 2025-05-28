a5d676ae2026a6ca5e5e24d5bd19323a17ddf32b
Kallel181 <86331656+Kallel181@users.noreply.github.com>
2025-05-28 20:07:43 -0300

    Update project.ts

--- src/project.ts
+++ src/project.ts
@@ -2,9 +2,10 @@ import {makeProject} from '@motion-canvas/core';
 
 import intro from './scenes/intro?scene';
 import Slide1 from './scenes/Slide1?scene';
+import Slide2 from './scenes/Slide2?scene';
 
 import './global.css'; // <- import the css
 
 export default makeProject({
-  scenes: [intro,Slide1],
+  scenes: [intro,Slide1,Slide2],
 });
