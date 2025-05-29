f16996b3e65f3897fe085060fe7c4d46d43887a0
Kallel181 <86331656+Kallel181@users.noreply.github.com>
2025-05-29 17:01:37 -0300

    Update Slide3.tsx

--- src/scenes/Slide3.tsx
+++ src/scenes/Slide3.tsx
@@ -1,5 +1,5 @@
 import { Layout, makeScene2D, Rect, Img, Txt } from '@motion-canvas/2d';
-import { createRef, beginSlide, slideTransition, Direction} from '@motion-canvas/core';
+import { createRef, beginSlide, slideTransition, Direction, all} from '@motion-canvas/core';
 import { revealTextScramble } from './util/animations';
 
 import * as styles from './util/styles';
@@ -13,10 +13,20 @@ export default makeScene2D(function* (view) {
   const engRevRef = createRef<Txt>();
   const engRevTitleRef = createRef<Txt>();
 
+
+  const card1_ref = createRef<Rect>();
+  const card2_ref = createRef<Rect>();
+  const card3_ref = createRef<Rect>();
+
+  const arrow1_ref = createRef<Img>();
+  const arrow2_ref = createRef<Img>();
+
+  const disclamer_ref = createRef<Txt>();
+
   // Textos
   const title_final_text = "Por que usar Ofuscação e Diversificação?";
-
-  const engRev = "O principal objetivo das técnicas de ofuscação e diversificação é dificultar a engenharia reversa de software."
+  const engRev = "O principal objetivo das técnicas de ofuscação e diversificação é dificultar a engenharia reversa de software.";
+  const disclamer = "Em linguagens interpretadas a extração da lógica ocorre direto do código, sem desmontagem, uma vez que o código não é compilado. A ofuscação textual é a principal defesa.";
 
   // Número da página no canto inferior direito
   view.add(
@@ -59,7 +69,7 @@ export default makeScene2D(function* (view) {
         
         
         <Layout layout direction={'row'} alignItems={'center'} gap={20} alignSelf={'center'}>
-          <Rect layout direction={'row'} alignItems={'center'} stroke={'#de7c31'} radius={25} lineWidth={8} padding={20} gap={15}>
+          <Rect layout ref={card1_ref} direction={'row'} alignItems={'center'} stroke={'#de7c31'} radius={25} lineWidth={8} padding={20} gap={15} opacity={0}>
             <Img src={'src/scenes/assets/Binary.png'} width={100}/>
             <Txt
               {...styles.DefaultNormalText}
@@ -69,9 +79,9 @@ export default makeScene2D(function* (view) {
             />            
           </Rect>
 
-          <Img src={'src/scenes/assets/arrow.png'} width={100}/>
+          <Img src={'src/scenes/assets/arrow.png'} ref={arrow1_ref} width={100} opacity={0}/>
           
-          <Rect layout direction={'row'} alignItems={'center'} stroke={'#b0de31'} radius={25} lineWidth={8} padding={20} gap={15}>
+          <Rect layout ref={card2_ref} direction={'row'} alignItems={'center'} stroke={'#b0de31'} radius={25} lineWidth={8} padding={20} gap={15} opacity={0}>
             <Img src={'src/scenes/assets/Binary_cogs.png'} width={120}/>
             <Txt
               {...styles.DefaultNormalText}
@@ -81,9 +91,9 @@ export default makeScene2D(function* (view) {
             />            
           </Rect>
 
-          <Img src={'src/scenes/assets/arrow.png'} width={100}/>
+          <Img src={'src/scenes/assets/arrow.png'} ref={arrow2_ref} width={100} opacity={0}/>
           
-          <Rect layout direction={'row'} alignItems={'center'} stroke={'#31bbde'} radius={25} lineWidth={8} padding={20} gap={15}>
+          <Rect layout ref={card3_ref} direction={'row'} alignItems={'center'} stroke={'#31bbde'} radius={25} lineWidth={8} padding={20} gap={15} opacity={0}>
             <Img src={'src/scenes/assets/Binary_glass.png'} width={120}/>
             <Txt
               {...styles.DefaultNormalText}
@@ -93,6 +103,14 @@ export default makeScene2D(function* (view) {
             />            
           </Rect>
         </Layout>
+        <Layout layout direction="column" gap={20}>
+          <Txt
+            {...styles.DefaultNormalText}
+            ref={disclamer_ref}
+            text={disclamer}
+          />
+        </Layout>
+
 
       </Layout>
     </Layout>
@@ -106,6 +124,24 @@ export default makeScene2D(function* (view) {
 
   yield* beginSlide('Animation 2');
   yield* engRevRef().opacity(1, 0.5);
+
+  yield* beginSlide('Animation 3');
+  yield* card1_ref().opacity(1, 0.5);
+
+  yield* beginSlide('Animation 4');
+  yield* all(
+    arrow1_ref().opacity(1, 0.5),
+    card2_ref().opacity(1, 0.5)
+  );
+
+  yield* beginSlide('Animation 5');
+  yield* all(
+    arrow2_ref().opacity(1, 0.5),
+    card3_ref().opacity(1, 0.5)
+  );
+
+  yield* beginSlide('Animation 6');
+  yield* disclamer_ref().opacity(1, 0.5);
 
 
 
